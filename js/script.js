76a6f039f8d2978ed36a751940ed7ea3e396c2d7
MichaelMantovani <michmantovani@gmail.com>
2023-06-06 16:29:17 +0200

    Collect DOM element for palindrome exercise

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 console.log('JS OK')
+
+
+// Recupero gli elementi dal DOM
+const palindromeInput = document.getElementById('palindrome');
+const palindromeButton = document.getElementById('btn-palindrome');
+const palindromeResultPlaceholder = document.querySelector('.word .result');
