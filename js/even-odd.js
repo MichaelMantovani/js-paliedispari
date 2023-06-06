32fbc7564bd4ce6779f9c57cec16b8a83213aae0
MichaelMantovani <michmantovani@gmail.com>
2023-06-06 17:52:33 +0200

    Collect numbers games DOM elements

--- js/even-odd.js
+++ js/even-odd.js
@@ -1,1 +1,11 @@
 console.log('JS OK')
+
+// Recupero gli elementi dal DOM
+const userChoice = document.getElementById('even-odd');
+const numberInput = document.getElementById('number');
+const resultPlaceholder = document.querySelector('.number-game .result')
+const numberButton = document.getElementById('btn-even-odd')
+
+
+
+
