1cf33d6138a7aaada6f926609e9a8e5f06c93502
MichaelMantovani <michmantovani@gmail.com>
2023-06-06 17:10:01 +0200

    Add is palindrome function

--- js/script.js
+++ js/script.js
@@ -5,3 +5,26 @@ console.log('JS OK')
 const palindromeInput = document.getElementById('palindrome');
 const palindromeButton = document.getElementById('btn-palindrome');
 const palindromeResultPlaceholder = document.querySelector('.word .result');
+
+
+// Creo una funzione di controllo per verificare se la parola è palindroma
+function isPalindrome(word){
+  const wordLetters = word.split('')
+  
+  let reversedWord = ''
+  for(let i = wordLetters.length - 1; i >= 0; i--) {
+    reversedWord += wordLetters[i]
+  }
+
+
+  let isValid = false
+  if (reversedWord === word) isValid = true 
+
+
+  return isValid;
+}
+
+
+
+
+
