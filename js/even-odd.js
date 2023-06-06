160e6731c22e522926a2dc614526d0d2cfcfcbef
MichaelMantovani <michmantovani@gmail.com>
2023-06-06 17:58:26 +0200

    Create random number function

--- js/even-odd.js
+++ js/even-odd.js
@@ -6,6 +6,13 @@ const numberInput = document.getElementById('number');
 const resultPlaceholder = document.querySelector('.number-game .result')
 const numberButton = document.getElementById('btn-even-odd')
 
+// Creo una funzione che genera un numero randomico da 1 a 5  
+function getRandomNumber (min,max){
+  const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+  return randomNumber;
+}
+
+
 
 
 
